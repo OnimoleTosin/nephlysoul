6a9e896fce4ef4dc1f25d2af49cdfd593feb6756
Onimole Sodiq <onimolesodiq282@gmail.com>
2025-06-17 17:27:58 +0100

    Featured Articles

--- app/page.js
+++ app/page.js
@@ -1,29 +1,12 @@
 import React from 'react'
-import Navbar from "@/components/Navbar"
-import Slider from "@/components/Slider"
-import Footer from '@/components/Footer'
-import Quotes from "@/components/Quotes"
-import QuickActions from "@/components/QuickActions"
-import FeaturedBooks from '@/components/FeaturedBooks'
-import PopularGroups from "@/components/PopularGroups"
-import Testimony from "@/components/Testimony"
-import FeaturedArticles from '@/components/FeaturedArticles'
+import HomePage from './HomePage'
+
 
 const page = () => {
   return (
-    <>
-      <Navbar />
-      <div>
-        <Slider />
-        <QuickActions />
-        <FeaturedArticles />
-        <FeaturedBooks />
-        <Quotes />
-        <PopularGroups />
-        <Testimony />
-      </div>
-      <Footer />
-    </>
+    <div>
+      <HomePage/>
+    </div>
   )
 }
 
